a036a9e2ac7c08426ebc16838e162199926ea9f0
Heinrich Harms <freelanceharms@gmail.com>
2022-02-26 18:05:24 -0600

    added testing code for fft function

--- fct.cpp
+++ fct.cpp
@@ -2,10 +2,12 @@
 #include <complex>
 #include <bits/stdc++.h>
 #include <cmath>
+#include <math.h>
 
 using std::complex;
 using std::exp;
 using std::real;
+using std::cos;
 using std::cout;
 
 
@@ -26,7 +28,7 @@ inum* calculate_fourier_twiddles(int N) {
 
 inum* fft(const inum* x, int N, int s, const inum* fourier_twiddles) {
     if (N == 1) {
-        return new inum(v[0]);
+        return new inum(x[0]);
     }
     inum* X = new inum[N];
     int new_N = N/2;
@@ -45,7 +47,7 @@ inum* fft(const inum* x, int N, int s, const inum* fourier_twiddles) {
 inum* calculate_cosine_twiddles(int N) {
     inum* cosine_twiddles;
     for (int k=0; k<N; k++) {
-        cosine_twiddles[k] = 2 * exp(-i * pi * double(k) / (2.0 * N));
+        cosine_twiddles[k] = 2.0 * exp(-i * pi * double(k) / (2.0 * N));
     }
     return cosine_twiddles;
 }
@@ -58,12 +60,108 @@ inum* calculate_cosine_twiddles(int N) {
 
 int main() {
     int N = 16;
-    inum* data = new inum[N];
+    inum* input_data = new inum[N];
+    inum* output_data = new inum[N];
 
-    // test for if all nums are 0.0
+    // I got the tests from here:
+    // https://www.stackoverflow.com/questions/25298119/unit-testing-a-discrete-fourier-transformation
+    // specifically referencing:
+    //      https://www.dsprelated.com/showthread/comp.dsp/71595-1.php
+
+    // calculate twiddle factors for fourier transform
+    inum* fourier_twiddles = calculate_fourier_twiddles(N);
+
+    // TEST 1
+    cout << "TEST 1:\n\tInput Data:\t";
     for (int k=0; k<N; k++) {
-        data[k] = 0.0;
+        input_data[k] = 0.0;
+        cout << '|' << input_data[k];
     }
+    cout << "|\n\tOutput Data:\t";
+    output_data = fft(input_data, N, 1, fourier_twiddles);
+    for (int k=0; k<N; k++) {
+        cout << '|' << output_data[k];
+    }
+    cout << "|\n\n";
 
+    // TEST 2
+    cout << "TEST 2:\n\tInput Data:\t";
+    for (int k=0; k<N; k++) {
+        input_data[k] = 1.0;
+        cout << '|' << input_data[k];
+    }
+    cout << "|\n\tOutput Data:\t";
+    output_data = fft(input_data, N, 1, fourier_twiddles);
+    for (int k=0; k<N; k++) {
+        cout << '|' << output_data[k];
+    }
+    cout << "|\n\n";
+
+    // TEST 3
+    cout << "TEST 3:\n\tInput Data:\t";
+    for (int k=0; k<N; k++) {
+        input_data[k] = 1.0 - k % 2 * 2;
+        cout << '|' << input_data[k];
+    }
+    cout << "|\n\tOutput Data:\t";
+    output_data = fft(input_data, N, 1, fourier_twiddles);
+    for (int k=0; k<N; k++) {
+        cout << '|' << output_data[k];
+    }
+    cout << "|\n\n";
+
+    // TEST 4
+    cout << "TEST 4:\n\tInput Data:\t";
+    for (int k=0; k<N; k++) {
+        input_data[k] = exp(16 * pi * i * double(k) / double(N));
+        cout << '|' << input_data[k];
+    }
+    cout << "|\n\tOutput Data:\t";
+    output_data = fft(input_data, N, 1, fourier_twiddles);
+    for (int k=0; k<N; k++) {
+        cout << '|' << output_data[k];
+    }
+    cout << "|\n\n";
+
+    // TEST 5
+    cout << "TEST 5:\n\tInput Data:\t";
+    for (int k=0; k<N; k++) {
+        input_data[k] = cos(16 * pi * k / N);
+        cout << '|' << input_data[k];
+    }
+    cout << "|\n\tOutput Data:\t";
+    output_data = fft(input_data, N, 1, fourier_twiddles);
+    for (int k=0; k<N; k++) {
+        cout << '|' << output_data[k];
+    }
+    cout << "|\n\n";
+
+    // TEST 6
+    cout << "TEST 6:\n\tInput Data:\t";
+    for (int k=0; k<N; k++) {
+        input_data[k] = exp((double(43) / 7) * i * 2.0 * pi * double(k) / double(N));
+        cout << '|' << input_data[k];
+    }
+    cout << "|\n\tOutput Data:\t";
+    output_data = fft(input_data, N, 1, fourier_twiddles);
+    for (int k=0; k<N; k++) {
+        cout << '|' << output_data[k];
+    }
+    cout << "|\n\n";
+
+    // TEST 7
+    cout << "TEST 7:\n\tInput Data:\t";
+    for (int k=0; k<N; k++) {
+        input_data[k] = cos((double(43) / 7) * 2 * pi * k / N);
+        cout << '|' << input_data[k];
+    }
+    cout << "|\n\tOutput Data:\t";
+    output_data = fft(input_data, N, 1, fourier_twiddles);
+    for (int k=0; k<N; k++) {
+        cout << '|' << output_data[k];
+    }
+    cout << "|\n\n";
+
+    // return successful exit code
     return 0;
 }
